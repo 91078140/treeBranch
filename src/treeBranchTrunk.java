c7d3dad07a7a5fe7ef46b486c6476c58135ea1b8
91078140 <91078140@mvusd.net>
2020-03-09 13:23:32 -0700

    Added the lorax job.

--- src/treeBranchTrunk.java
+++ src/treeBranchTrunk.java
@@ -3,5 +3,6 @@ public class treeBranchTrunk
     public static void main(String[] args)
     {
        System.out.println("Trees are cool?");
+       System.out.println("The Lorax speaks for the trees.");
     }
 }// end of driver class

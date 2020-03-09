bdfccdb804b96a89ba44c4558fb8464c015edcb8
91078140 <91078140@mvusd.net>
2020-03-09 13:29:03 -0700

    Added fluffy trees

--- src/treeBranchTrunk.java
+++ src/treeBranchTrunk.java
@@ -4,5 +4,6 @@ public class treeBranchTrunk
     {
        System.out.println("Trees are cool?");
        System.out.println("The Lorax speaks for the trees.");
+       System.out.println("The trees are fluffy.");
     }
 }// end of driver class
